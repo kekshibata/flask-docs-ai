097a03f3c83bfaef531aba04b5989c9dbe3aee9c
kekshibata <kenergyintense@gmail.com>
2023-03-31 10:26:28 +0900

    pdf as image

--- backend/app.py
+++ backend/app.py
@@ -11,24 +11,16 @@ CORS(app)
 UPLOAD_FOLDER = "uploads"
 
 
-def process_pdf(project_id, location, processor_id, file):
+def process_pdf(project_id, location, processor_id, file_path):
     client = documentai.DocumentProcessorServiceClient()
 
     # The full resource name of the processor, e.g.:
     # projects/project-id/locations/location/processors/processor-id
     name = client.processor_path(project_id, location, processor_id)
 
-    with io.BytesIO() as buf:
-        buf.write(file.read())
-        # input_config = {
-        #     "gcs_source": {
-        #         "uri": f"gs://{gcs_bucket}/{gcs_file_path}"
-        #     },
-        #     "mime_type": "application/pdf"
-        # }
-        # Read the file into memory
-        buf.seek(0)
-        raw_document = documentai.RawDocument(content=buf.read(), mime_type="application/pdf")
+    with open(file_path, "rb") as image:
+        image_content = image.read()
+        raw_document = documentai.RawDocument(content=image_content, mime_type="application/pdf")
 
         # request_ = {"name": name, "document": document}
         request_ = documentai.ProcessRequest(name=name, raw_document=raw_document)
@@ -36,6 +28,7 @@ def process_pdf(project_id, location, processor_id, file):
 
         # Extract and return the text from the document
         return result.document.text
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -45,14 +38,15 @@ def upload_file():
     files = request.files.getlist("files")
     for file in files:
         filename = secure_filename(file.filename)
-        file.save(os.path.join(UPLOAD_FOLDER, filename))
+        file_path = os.path.join(UPLOAD_FOLDER, filename)
+        file.save(file_path)
 
         if file.filename.lower().endswith(".pdf"):
             # Replace with your project ID, location, and processor ID
             project_id = os.environ.get("PROJECT_ID")
             location = os.environ.get("LOCATION")
             processor_id = os.environ.get("PROCESSOR_ID")
-            text = process_pdf(project_id, location, processor_id, file)
+            text = process_pdf(project_id, location, processor_id, file_path)
             return jsonify({"message": "PDF processed", "content": text}), 200
 
     return {"message": "Files uploaded successfully"}, 200
